7bdc8c402a78f3f61e07d47db3f29eb19435a729
xiaowenbin <xiaowenbin@ylmf1000.onexmail.com>
2022-01-28 18:11:46 +0800

    feat：spring study

---
+++ ch08-spring-trans/src/main/java/org/example/service/BuyGoodsService.java
@@ -0,0 +1,6 @@
+package org.example.service;
+
+public interface BuyGoodsService {
+    //goodsId:商品编号，nums：购买的数量
+    void buy(Integer goodsId,Integer nums);
+}
---
+++ ch08-spring-trans/src/main/java/org/example/service/impl/BuyGoodsServiceImpl.java
@@ -0,0 +1,10 @@
+package org.example.service.impl;
+
+import org.example.service.BuyGoodsService;
+
+public class BuyGoodsServiceImpl implements BuyGoodsService {
+    @Override
+    public void buy(Integer goodsId, Integer nums) {
+
+    }
+}
